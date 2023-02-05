05b0480b3821088aba8ae151938b02d10522f485
Maxim Pavlenko 77 <max.pavlenko@gmail.com>
2023-02-05 21:19:34 +0300

    API Task 1 completed

--- API_Netology_Pavlenko.py
+++ API_Netology_Pavlenko.py
@@ -1,17 +1,9 @@
 
 import requests
-from datetime import *
 
-epoch = date(1970, 1, 1)
-current_date = date.today()
-past_date = date.today()-timedelta(days=2)
-start_date = int((past_date-epoch).total_seconds())
-end_date = str(int((current_date -epoch).total_seconds()))
+url = 'https://akabab.github.io/superhero-api/api/all.json'
 
-url=f'https://api.stackexchange.com/2.3/questions?fromdate={start_date}&todate={end_date}&tagged=Python&site=stackoverflow'
+data = requests.get(url).json()
+result = {key["name"]:key["powerstats"]["intelligence"] for key in data if key["name"]=="Hulk" or key["name"]=="Captain America" or key["name"]=="Thanos"}
 
-result = requests.get(url).json()
-result_list = [q['title'] for q in result['items']]
-for index, item in enumerate(result_list,1):
-    print (index, item)
-    print()
+print(f'Самый умный супергерой из трех: {max(result, key= lambda x: result[x])}')
