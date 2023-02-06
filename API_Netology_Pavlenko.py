e4cf0eb2236041901ed5214b1d76bb1baa8d0449
Maxim Pavlenko 77 <max.pavlenko@gmail.com>
2023-02-06 22:56:48 +0300

    Task 2 final

--- API_Netology_Pavlenko.py
+++ API_Netology_Pavlenko.py
@@ -1,16 +1,35 @@
-﻿class YaUploader:
+﻿
+import requests
+
+class YaUploader:
     def __init__(self, token: str):
         self.token = token
+    
 
-    def upload(self, file_path: str):
-        """Метод загружает файлы по списку file_list на яндекс диск"""
-        # Тут ваша логика
-        # Функция может ничего не возвращать
+    def _get_upload_link(self, file_path: str):
+        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+        headers = {
+            'Content-Type':'application/json',
+            'Authorization':'OAuth {}'.format(self.token)
+            }
+        params = {"path":file_path, "overwrite":"true"}
+        response = requests.get(upload_url, headers=headers, params=params)
+        return response.json()
+
+    def upload_file_to_disk(self, file_path, file_name):
+        href = self._get_upload_link(file_path=file_path).get("href","")
+        response = requests.put(href, data=open(file_name,'rb'))
+        return response
 
 
 if __name__ == '__main__':
-    # Получить путь к загружаемому файлу и токен от пользователя
-    path_to_file = ...
-    token = ...
+    
+    path_to_file = input('Введите путь до файла:')
+    token = input('Введите токен:')
     uploader = YaUploader(token)
-    result = uploader.upload(path_to_file)
+    result = uploader.upload_file_to_disk(file_path="Загрузки/test0055.txt", file_name=path_to_file)
+    
+    if result.status_code == 201:
+        print('Файл успешно загружен')
+    else:
+        print(f'Загрузка файла неуспешна, ответ сервера {result.status_code}')
